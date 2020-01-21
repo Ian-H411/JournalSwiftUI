0d440ec8a11037d39f648cc4e67f8d8e375bba84
Ian_H411 <ian.h411@gmail.com>
2020-01-20 20:22:24 -0600

    adds swiftUI Files

---
+++ JournalSwiftUI2/JournalSwiftUI2/UI/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  JournalSwiftUI2
+//
+//  Created by Ian Hall on 1/20/20.
+//  Copyright © 2020 Ian Hall. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
